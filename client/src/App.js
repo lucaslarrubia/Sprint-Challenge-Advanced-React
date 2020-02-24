3a8bc2b187d57d152781f805dce0c6af8d15e836
lucaslarrubia <Lucas@Marcias-iMac.attlocal.net>
2020-02-23 20:10:31 -0500

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import PlayerCard from './PlayerCard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => this.setState({ players: res.data }))
+    .catch(err => console.log(err));
+  }
+
+  render () {
+    console.log('Do I have players??', this.state.players);
+     return (
+      <div className="App">
+        <PlayerCard players={this.state.players} />
+      </div>
+    );
+  }
 }
 
 export default App;
